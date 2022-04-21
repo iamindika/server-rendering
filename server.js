8528edf068a010d6a6effac6ab83eb855b8a27c7
iamindika <ikolamba2@gmail.com>
2022-04-21 12:33:05 -0400

    utilize routes and match to optionally fetch repo data

--- server.js
+++ server.js
@@ -94,9 +94,15 @@ var _App = __webpack_require__(5);
 
 var _App2 = _interopRequireDefault(_App);
 
-var _serializeJavascript = __webpack_require__(6);
+var _serializeJavascript = __webpack_require__(7);
 
 var _serializeJavascript2 = _interopRequireDefault(_serializeJavascript);
+
+var _reactRouterDom = __webpack_require__(10);
+
+var _routes = __webpack_require__(11);
+
+var _routes2 = _interopRequireDefault(_routes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -107,11 +113,17 @@ app.use((0, _cors2.default)());
 app.use(_express2.default.static('public'));
 
 app.get('*', function (req, res, next) {
-  var name = 'Scruffy';
+  var activeRoute = _routes2.default.find(function (route) {
+    return (0, _reactRouterDom.matchPath)(req.url, route);
+  }) || {};
 
-  var markup = (0, _server.renderToString)(_react2.default.createElement(_App2.default, { data: name }));
+  var promise = activeRoute.fetchInitialData ? activeRoute.fetchInitialData(req.path) : Promise.resolve();
 
-  res.send('\n    <!DOCTYPE html>\n    <html>\n      <head>\n        <title>SSR with RR</title>\n        <script src=\'/bundle.js\' defer></script>\n        <script>\n          window.__INITIAL_DATA__ = ' + (0, _serializeJavascript2.default)(name) + '\n        </script>\n      </head>\n      <body>\n        <div id=\'app\'>' + markup + '</div>\n      </body>\n    </html>\n  ');
+  promise.then(function (data) {
+    var markup = (0, _server.renderToString)(_react2.default.createElement(_App2.default, { data: data }));
+
+    res.send('\n      <!DOCTYPE html>\n      <html>\n        <head>\n          <title>SSR with RR</title>\n          <script src=\'/bundle.js\' defer></script>\n          <script>\n            window.__INITIAL_DATA__ = ' + (0, _serializeJavascript2.default)(data) + '\n          </script>\n        </head>\n        <body>\n          <div id=\'app\'>' + markup + '</div>\n        </body>\n      </html>\n    ');
+  }).catch(next);
 });
 app.listen(PORT, function () {
   console.log('Server is listening on port: ' + PORT);
@@ -152,6 +164,10 @@ var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
+var _Grid = __webpack_require__(6);
+
+var _Grid2 = _interopRequireDefault(_Grid);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -173,11 +189,9 @@ var App = function (_Component) {
     key: 'render',
     value: function render() {
       return _react2.default.createElement(
-        'h1',
+        'div',
         null,
-        'Hello ',
-        this.props.data,
-        '!'
+        _react2.default.createElement(_Grid2.default, { data: this.props.data })
       );
     }
   }]);
@@ -189,9 +203,204 @@ exports.default = App;
 
 /***/ }),
 /* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Grid = function (_Component) {
+  _inherits(Grid, _Component);
+
+  function Grid() {
+    _classCallCheck(this, Grid);
+
+    return _possibleConstructorReturn(this, (Grid.__proto__ || Object.getPrototypeOf(Grid)).apply(this, arguments));
+  }
+
+  _createClass(Grid, [{
+    key: 'render',
+    value: function render() {
+      var repos = this.props.data;
+
+      return _react2.default.createElement(
+        'ul',
+        { style: { display: 'flex', 'flexWrap': 'wrap' } },
+        repos.map(function (_ref) {
+          var name = _ref.name,
+              owner = _ref.owner,
+              stargazers_count = _ref.stargazers_count,
+              html_url = _ref.html_url;
+          return _react2.default.createElement(
+            'li',
+            { key: html_url, style: { margin: 30 } },
+            _react2.default.createElement(
+              'ul',
+              null,
+              _react2.default.createElement(
+                'li',
+                null,
+                _react2.default.createElement(
+                  'a',
+                  { href: html_url },
+                  name
+                )
+              ),
+              _react2.default.createElement(
+                'li',
+                null,
+                '@',
+                owner.login
+              ),
+              _react2.default.createElement(
+                'li',
+                null,
+                stargazers_count,
+                ' stars'
+              )
+            )
+          );
+        })
+      );
+    }
+  }]);
+
+  return Grid;
+}(_react.Component);
+
+exports.default = Grid;
+
+/***/ }),
+/* 7 */
 /***/ (function(module, exports) {
 
 module.exports = require("serialize-javascript");
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.fetchPopularRepos = fetchPopularRepos;
+
+var _isomorphicFetch = __webpack_require__(9);
+
+var _isomorphicFetch2 = _interopRequireDefault(_isomorphicFetch);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function fetchPopularRepos() {
+  var language = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'all';
+
+  var encodedURI = encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
+
+  return (0, _isomorphicFetch2.default)(encodedURI).then(function (data) {
+    return data.json();
+  }).then(function (repos) {
+    return repos.items;
+  }).catch(function (error) {
+    console.warn(error);
+    return null;
+  });
+}
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports) {
+
+module.exports = require("isomorphic-fetch");
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports) {
+
+module.exports = require("react-router-dom");
+
+/***/ }),
+/* 11 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Home = __webpack_require__(12);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+var _Grid = __webpack_require__(6);
+
+var _Grid2 = _interopRequireDefault(_Grid);
+
+var _api = __webpack_require__(8);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var routes = [{
+  path: '/',
+  exact: true,
+  component: _Home2.default
+}, {
+  path: '/popular/:id',
+  component: _Grid2.default,
+  fetchInitialData: function fetchInitialData() {
+    var path = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+    return (0, _api.fetchPopularRepos)(path.split('/').pop());
+  }
+}];
+
+exports.default = routes;
+
+/***/ }),
+/* 12 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = Home;
+
+var _react = __webpack_require__(0);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Home() {
+  return _react2.default.createElement(
+    'div',
+    null,
+    'Select a language'
+  );
+}
 
 /***/ })
 /******/ ]);
--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import App from '../shared/App';
-import serialize from 'serialize-javascript';
-import { getPopularRepos } from '../shared/api'
+import App from '../shared/App'
+import serialize from 'serialize-javascript'
+import { matchPath } from 'react-router-dom'
+import routes from '../shared/routes'
 
 const PORT = 3000
 const app = express()
@@ -13,28 +14,33 @@ app.use(cors())
 app.use(express.static('public'))
 
 app.get('*', (req, res, next) => {
-  getPopularRepos()
-    .then((data) => {
-      const markup = renderToString(
-        <App data={data}/>
-      )
-    
-      res.send(`
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <title>SSR with RR</title>
-            <script src='/bundle.js' defer></script>
-            <script>
-              window.__INITIAL_DATA__ = ${serialize(data)}
-            </script>
-          </head>
-          <body>
-            <div id='app'>${markup}</div>
-          </body>
-        </html>
-      `)
-    })
+  const activeRoute = routes.find((route) => matchPath(req.url, route)) || {}
+
+  const promise = activeRoute.fetchInitialData
+    ? activeRoute.fetchInitialData(req.path)
+    : Promise.resolve()
+
+  promise.then((data) => {
+    const markup = renderToString(
+      <App data={data}/>
+    )
+  
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>SSR with RR</title>
+          <script src='/bundle.js' defer></script>
+          <script>
+            window.__INITIAL_DATA__ = ${serialize(data)}
+          </script>
+        </head>
+        <body>
+          <div id='app'>${markup}</div>
+        </body>
+      </html>
+    `)
+  }).catch(next)
 })
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`)
